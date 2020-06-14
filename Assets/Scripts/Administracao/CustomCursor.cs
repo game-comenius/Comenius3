c7ca0376f3f99640f3876600d7ff28b091f3117b
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-06-14 17:06:16 -0300

    Criar GameObject Admin para todas as cenas
     - Um GameObject chamado Admin estará presente em todas as cenas do jogo.
       Geralmente, ele será um grupo de scripts "Don't Destroy" que monitoram
       o game comenius 3. Por exemplo, scripts que controlam o estado do jogo
       e scripts que controlam a imagem .png do cursor.
     - Este commit contém muitos outros arquivos novos e atualizados, não
       consegui separar em um commit novo de um jeito fácil, eu peço desculpas
    C#	new file:   Assets/Scripts/Utilitarios/OnHoverCursorChanger.cs.meta

---
+++ Assets/Scripts/Administracao/CustomCursor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CustomCursor : Singleton<CustomCursor>
+{
+    public Texture2D textureImage;
+    public Vector2 hotSpot = Vector2.zero;
+
+    [HideInInspector] public readonly CursorMode cursorMode = CursorMode.Auto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (textureImage == null)
+            Debug.LogWarning("Texture2D para CustomCursor não está definida, defina no inspector para alterar sprite do cursor.");
+        else
+            Cursor.SetCursor(textureImage, hotSpot, cursorMode);
+    }
+}
---
+++ Assets/Scripts/Utilitarios/OnHoverCursorChanger.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnHoverCursorChanger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    // The PNG image of the cursor when it's hovering something is at
+    // "Assets/Resources/Sprites/Cursores/OnHoverCursor32px.png"
+    // We will load it using Resources.Load the first time the property is accessed
+    private static Texture2D textureImageWhenHovering;
+    public static Texture2D TextureImageWhenHovering
+    {
+        get
+        {
+            if (!textureImageWhenHovering)
+                textureImageWhenHovering = Resources.Load<Texture2D>("Sprites/Cursores/OnHoverCursor32px");
+            return textureImageWhenHovering;
+        }
+    }
+    private static Vector2 hotSpot = Vector2.zero;
+    private static CursorMode cursorMode = CursorMode.Auto;
+
+    // Fields that represent the cursor when it is not hovering something
+    // These values come from the Singleton instance of CustomCursor.cs
+    // We will use these when we change the cursor back to its standard form,
+    // for example, when it is no longer hovering this GameObject
+    private Texture2D standardTextureImage;
+    private Vector2 standardHotSpot;
+    private CursorMode standardCursorMode;
+
+
+    void Start()
+    {
+        var customCursor = CustomCursor.Instance;
+        standardTextureImage = customCursor.textureImage;
+        standardHotSpot = customCursor.hotSpot;
+        standardCursorMode = customCursor.cursorMode;
+    }
+
+    private void ChangeCursorToOnHover()
+    {
+        Cursor.SetCursor(TextureImageWhenHovering, hotSpot, cursorMode);
+    }
+
+    private void ChangeCursorBackToStandard()
+    {
+        Cursor.SetCursor(standardTextureImage, standardHotSpot, standardCursorMode);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ChangeCursorToOnHover();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ChangeCursorBackToStandard();
+    }
+
+    // Transforms cursor pointer back to its standard version when this
+    // object is destroyed or disabled
+    private void OnDestroy() { ChangeCursorBackToStandard(); }
+    private void OnDisable() { ChangeCursorBackToStandard(); }
+}
