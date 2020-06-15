64fb18827a3e6e465e7236b4b22c12b3303410bd
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-06-14 23:14:24 -0300

    Adicionar links nos botões do menu principal

---
+++ Assets/Scripts/Utilitarios/UrlOpener.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UrlOpener : MonoBehaviour {
+
+    public void Open(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
