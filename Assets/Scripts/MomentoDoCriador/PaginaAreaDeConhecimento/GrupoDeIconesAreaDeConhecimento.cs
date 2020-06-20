305fdc9b2081e7ad1c76277d618ecc62cd628150
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-06-20 17:02:35 -0300

    Criar vínculo entre nível de ensino e ícones

---
+++ Assets/Scripts/MomentoDoCriador/PaginaAreaDeConhecimento/GrupoDeIconesAreaDeConhecimento.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrupoDeIconesAreaDeConhecimento : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Esconder();
+    }
+
+    public void Mostrar()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void Esconder()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/MomentoDoCriador/PaginaAreaDeConhecimento/PaginaAreaDeConhecimento.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaginaAreaDeConhecimento : Pagina
+{
+    [SerializeField] GrupoDeIconesAreaDeConhecimento iconesInfantil;
+    [SerializeField] GrupoDeIconesAreaDeConhecimento iconesFundamental;
+    [SerializeField] GrupoDeIconesAreaDeConhecimento iconesMedio;
+    [SerializeField] GrupoDeIconesAreaDeConhecimento iconesSuperior;
+
+    private Dictionary<NivelDeEnsino, GrupoDeIconesAreaDeConhecimento> IconesPorNivelDeEnsino;
+
+    private GrupoDeIconesAreaDeConhecimento grupoAtual;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Criar mapeamento entre nível de ensino e os respectivos ícones
+        IconesPorNivelDeEnsino = new Dictionary<NivelDeEnsino, GrupoDeIconesAreaDeConhecimento>();
+        IconesPorNivelDeEnsino[NivelDeEnsino.EducacaoInfantil] = iconesInfantil;
+        IconesPorNivelDeEnsino[NivelDeEnsino.EnsinoFundamental] = iconesFundamental;
+        IconesPorNivelDeEnsino[NivelDeEnsino.EnsinoMedio] = iconesMedio;
+        IconesPorNivelDeEnsino[NivelDeEnsino.EnsinoSuperior] = iconesSuperior;
+    }
+
+    public override void Mostrar()
+    {
+        base.Mostrar();
+
+        ApresentarGrupoDeIcones(EstadoDoJogo.Instance.NivelDeEnsinoSelecionado);
+    }
+
+    public override bool Validar()
+    {
+        return base.Validar();
+    }
+
+    private void ApresentarGrupoDeIcones(NivelDeEnsino nivelDeEnsino)
+    {
+        if (grupoAtual) grupoAtual.Esconder();
+        grupoAtual = IconesPorNivelDeEnsino[nivelDeEnsino];
+        grupoAtual.Mostrar();
+    }
+}
