38f28edf141dd14dea1a2a915c724d6937aded80
AlexisMS <alexisora@gmail.com>
2020-06-24 03:24:00 -0300

    Iniciando implementação de mídias

---
+++ Assets/Scripts/Itens/Midia.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Midias
+{
+    public NomeMidias NomeMidia { get; private set; }
+    public Sprite Sprite { get; private set; }
+
+    public string NomeApresentavel { get; private set; }
+    public string Descricao { get; private set; }
+
+    public Midias (NomeMidias nomeMidia)
+    {
+        NomeMidia = nomeMidia;
+
+        switch (NomeMidia)
+        {
+            case NomeMidias.QuadroNegro:
+                NomeApresentavel = "Quadro Negro";
+                Descricao = "É um quadro negro.";
+                break;
+            case NomeMidias.Caderno:
+                NomeApresentavel = "Caderno";
+                Descricao = "É um caderno";
+                break;
+            default:
+                NomeApresentavel = "Sem nome";
+                Descricao = "Sem texto";
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Itens/NomeMidias.cs
@@ -0,0 +1,6 @@
+﻿
+public enum NomeMidias
+{
+    QuadroNegro,
+    Caderno
+}
