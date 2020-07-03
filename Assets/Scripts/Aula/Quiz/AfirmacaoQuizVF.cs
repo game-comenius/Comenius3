f23342471596bad6482dd09e743216c083e624b3
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-07-03 17:47:36 -0300

    Atualização para a reunião do dia 03.07.2020

--- Assets/Scripts/Aula/Quiz/AfirmacaoQuizVF.cs
+++ Assets/Scripts/Aula/Quiz/AfirmacaoQuizVF.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class AfirmacaoQuizVF : MonoBehaviour
+public class AfirmacaoQuizVF : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] Text textoAfirmacao;
+
+    [SerializeField] Sprite spriteNaoSelecionada;
+    [SerializeField] Sprite spriteSelecionada;
+    [SerializeField] Sprite spriteCorreta;
+    [SerializeField] Sprite spriteErrada;
 
     public string Texto
     {
         get { return textoAfirmacao.text; }
         set { textoAfirmacao.text = value;}
+    }
+
+    public bool Selecionada { get; set; }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Selecionada = !Selecionada;
+
+        GetComponent<Image>().sprite = (Selecionada) ? spriteSelecionada : spriteNaoSelecionada;
     }
 }
