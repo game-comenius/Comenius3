71dc36c24350da2978d3c5912530d20fe7dbea32
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-07-03 18:09:45 -0300

    Trocar de "Reprodutor ..." para "Aparelho de Som"

--- Assets/Scripts/Itens/NomeMidias.cs
+++ Assets/Scripts/Itens/NomeMidias.cs
@@ -8,7 +8,7 @@ public enum NomeMidias
     LivrosJornaisERevistas,
     CadernosECartazes,
     Televisao,
-    ReprodutorDeAudio,
+    AparelhoDeSom,
     RedesSociais,
     Jogos,
     EditoresDeAudioEVideo,
