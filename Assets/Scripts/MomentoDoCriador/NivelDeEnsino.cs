8ca3664be48586df2db9096a3ad875cb7ec67282
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-07-06 19:05:13 -0300

    Adicionar descrições dos níveis de ensino

--- Assets/Scripts/MomentoDoCriador/NivelDeEnsino.cs
+++ Assets/Scripts/MomentoDoCriador/NivelDeEnsino.cs
@@ -6,9 +6,10 @@ public sealed class NivelDeEnsino
 {
     public static readonly NivelDeEnsino EducacaoInfantil = new NivelDeEnsino
     (
-        0,
-        "Educação Infantil",
-        new int[]
+        valor: 0,
+        nome: "Educação Infantil",
+        descricao: "É a primeira etapa da Educação Básica tendo os princípios de educar e cuidar como indissociáveis buscando o objetivo de ampliar o universo de experiências, conhecimentos e habilidades de bebês e crianças através da parceria com a família. É oferecida em creches para crianças de até 3 anos de idade em pré-escolas para crianças de 4 a 5 anos (sendo esta obrigatória a matrícula). Tem como eixos estruturantes das práticas pedagógicas as interações e a brincadeira por meio de intencionalidade educativa e a garantia de seis direitos de aprendizagem e desenvolvimento que permeiam cinco campos de experiências tendo como finalidade o desenvolvimento integral da criança até 5 anos.",
+        areasDeConhecimento: new int[]
         {
             AreaDeConhecimento.EuOutroNos.valor,
             AreaDeConhecimento.CorpoGestosMovimentos.valor,
@@ -20,9 +21,10 @@ public sealed class NivelDeEnsino
 
     public static readonly NivelDeEnsino EnsinoFundamental = new NivelDeEnsino
     (
-        1,
-        "Ensino Fundamental",
-        new int[]
+        valor: 1,
+        nome: "Ensino Fundamental",
+        descricao: "Etapa obrigatória com duração de 9 anos (dos 6 aos 14 anos), tendo como objetivo a formação básica do cidadão. É dividido em duas fases: Anos Iniciais (1º ao 5º ano) e Anos Finais (6º ao 9º ano), que inicia-se em torno dos interesses manifestados pelas crianças até desafios de maior complexidade. Sendo importante buscar o fortalecimento da autonomia dos estudantes nesse nível de ensino, desenvolvendo sua capacidade de aprendizagem tendo em vista a aquisição de conhecimentos e habilidades e a formação de atitudes e valores que se fundamentam a sociedade.",
+        areasDeConhecimento: new int[]
         {
             AreaDeConhecimento.Matematica.valor,
             AreaDeConhecimento.CienciasDaNatureza.valor,
@@ -34,9 +36,10 @@ public sealed class NivelDeEnsino
 
     public static readonly NivelDeEnsino EnsinoMedio = new NivelDeEnsino
     (
-        2,
-        "Ensino Médio",
-        new int[]
+        valor: 2,
+        nome: "Ensino Médio",
+        descricao: "Etapa final da Educação Básica tem como finalidades o aprofundamento dos conhecimentos adquiridos anteriormente, a preparação básica para o trabalho e a cidadania, a formação ética e o aprimoramento do pensamento crítico através da compreensão dos fundamentos científico-tecnológicos dos processos produtivos, relacionando a teoria com a prática através de 4 grandes áreas do conhecimento produzindo múltiplas culturas juvenis promovendo uma educação integral no que concerne os aspectos físicos, cognitivos e socioemocionais.",
+        areasDeConhecimento: new int[]
         {
             AreaDeConhecimento.Matematica.valor,
             AreaDeConhecimento.CienciasDaNatureza.valor,
@@ -47,9 +50,10 @@ public sealed class NivelDeEnsino
 
     public static readonly NivelDeEnsino EnsinoSuperior = new NivelDeEnsino
     (
-        3,
-        "Ensino Superior",
-        new int[]
+        valor: 3,
+        nome: "Ensino Superior",
+        descricao: "É o nível que busca o desejo de aperfeiçoamento cultural e profissional estimulando o conhecimento dos problemas do mundo presente, buscando prestar serviços especializados à comunidade através da pesquisa científica e tecnológica. Além de buscar o aprimoramento da educação básica mediante a formação e a capacitação de profissionais através de pesquisas pedagógicas e o desenvolvimento de atividades de extensão em prol do desenvolvimento da sociedade brasileira.",
+        areasDeConhecimento: new int[]
         {
             AreaDeConhecimento.CienciasSociaisAplicadas.valor,
             AreaDeConhecimento.CienciasDaSaude.valor,
@@ -78,15 +82,17 @@ public sealed class NivelDeEnsino
             return returnValues;
         }
     }
+    public readonly string Descricao;
     // Quando o jogador seleciona este quando aperta sobre o ícone que
     // representa ele, o ícone salva seu sprite nesta propriedade
     // para que o jogo possa apresentá-la depois em outras cenas
     public Sprite Sprite;
 
-    private NivelDeEnsino(int valor, string nome, int[] areasDeConhecimento)
+    private NivelDeEnsino(int valor, string nome, string descricao, int[] areasDeConhecimento)
     {
         this.valor = valor;
         this.nome = nome;
+        this.Descricao = descricao;
         this.areasDeConhecimento = areasDeConhecimento;
     }
 
