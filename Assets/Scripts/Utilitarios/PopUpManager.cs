b01080cacdf866100eb5fdc690d970a50c04cafd
NicoleAGugli <38013926+nicolegugli@users.noreply.github.com>
2020-10-01 19:18:28 -0300

    Script gerenciamento pop up
    Ainda tem algumas melhoras que preciso fazer.

---
+++ Assets/Scripts/Utilitarios/PopUpManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpManager : MonoBehaviour
+{
+
+    public GameObject InfoCanvas;
+
+    public void OpenPanel()
+    {
+        if(InfoCanvas != null)
+        {
+            InfoCanvas.SetActive(true);
+        }
+    }
+
+    public void QuitPanel()
+    {
+        Debug.Log("Fechou");
+        InfoCanvas.SetActive(false);
+    }
+}
