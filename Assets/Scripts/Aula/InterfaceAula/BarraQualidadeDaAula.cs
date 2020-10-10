59e271c4c243aedb7e51584906b7218ed7bdbac1
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-10-10 20:36:46 -0300

    Fornecer função crescer/diminuir da barra aula

---
+++ Assets/Scripts/Aula/InterfaceAula/BarraQualidadeDaAula.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class BarraQualidadeDaAula : MonoBehaviour
+{
+    private Slider slider;
+    private Image preenchimentoImage;
+
+    // Valor da barra é de 0 a 1
+    private float valor;
+    public float Valor
+    {
+        get => valor;
+        set
+        {
+            valor = value;
+            slider.value = value;
+        }
+    }
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+        preenchimentoImage = slider.fillRect.GetComponent<Image>();
+    }
+
+    // Barra começa vazia
+    void Start() { this.Valor = 0; }
+}
