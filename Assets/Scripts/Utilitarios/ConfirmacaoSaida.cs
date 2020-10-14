b16069dddd4d04c5d3fca834870e9c00d7c0ffd5
NicoleAGugli <38013926+nicolegugli@users.noreply.github.com>
2020-10-14 11:53:37 -0300

    Implementação da confirmação de saída para o menu principal

---
+++ Assets/Scripts/Utilitarios/ConfirmacaoSaida.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfirmacaoSaida : MonoBehaviour
+{
+    public GameObject PainelDeConfirmacao;
+
+    public void AbrirPainel()
+    {
+        if(PainelDeConfirmacao != null)
+        {
+            PainelDeConfirmacao.SetActive(true);
+        }
+    }
+
+    public void FecharPainel()
+    {
+        if(PainelDeConfirmacao != null)
+        {
+            PainelDeConfirmacao.SetActive(false);
+        }
+    }
+
+}
