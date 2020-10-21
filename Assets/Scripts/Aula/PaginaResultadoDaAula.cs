859fff9f0ce69d3ad181e43749b0bab3502ed250
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-10-21 12:02:45 -0300

    Implementar o início da página resultado aula

--- Assets/Scripts/Aula/PaginaResultadoDaAula.cs
+++ Assets/Scripts/Aula/PaginaResultadoDaAula.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
+using TMPro;
 
 public class PaginaResultadoDaAula : MonoBehaviour
 {
-    [SerializeField] Text textPontuacaoDaAula;
-    [SerializeField] Text textTaxaDeAcertoDosQuizzes;
+    [Header("Barras")]
+    [SerializeField] Slider barraQualidadeDaAula;
+    [SerializeField] Slider barraTaxaDeAcertoNosQuizzes;
 
     public bool EmUso => gameObject.activeSelf;
 
@@ -17,20 +19,25 @@ public class PaginaResultadoDaAula : MonoBehaviour
     public void Atualizar(float pontuacaoDaAula, Quiz[] quizzesDaAula)
     {
         AtualizarPontuacaoDaAula(pontuacaoDaAula);
-        AtualizarTaxaDeAcertoDosQuizzes(quizzesDaAula);
+        AtualizarTaxaDeAcertoNosQuizzes(quizzesDaAula);
     }
 
     private void AtualizarPontuacaoDaAula(float pontuacaoDaAula)
     {
-        textPontuacaoDaAula.text = (pontuacaoDaAula * 100).ToString("f") + "%";
+        barraQualidadeDaAula.value = pontuacaoDaAula;
+        //textPontuacaoDaAula.text = (pontuacaoDaAula * 100).ToString("f") + "%";
     }
 
-    private void AtualizarTaxaDeAcertoDosQuizzes(Quiz[] quizzesDaAula)
+    private void AtualizarTaxaDeAcertoNosQuizzes(Quiz[] quizzesDaAula)
     {
-        float taxaDeAcertoDosQuizzes = 0;
+        float taxaDeAcertoNosQuizzes = 0;
         if (quizzesDaAula.Any())
-            taxaDeAcertoDosQuizzes = quizzesDaAula.Sum((quiz) => quiz.TaxaDeAcerto) / quizzesDaAula.Length;
-        textTaxaDeAcertoDosQuizzes.text = (taxaDeAcertoDosQuizzes * 100).ToString("f") + "%";
+            taxaDeAcertoNosQuizzes = quizzesDaAula.Sum((quiz) => quiz.TaxaDeAcerto) / quizzesDaAula.Length;
+        barraTaxaDeAcertoNosQuizzes.value = taxaDeAcertoNosQuizzes;
+        // Mostrar taxa de acerto nos quizzes como texto e arredondada para cima
+        var textoTaxaDeAcertoNosQuizzes = barraTaxaDeAcertoNosQuizzes.GetComponentInChildren<TextMeshProUGUI>();
+        var taxaDeAcertoPorcentagemArredondada = Mathf.Ceil(taxaDeAcertoNosQuizzes * 100);
+        textoTaxaDeAcertoNosQuizzes.text = taxaDeAcertoPorcentagemArredondada + "%";
     }
 
     public void Mostrar()
