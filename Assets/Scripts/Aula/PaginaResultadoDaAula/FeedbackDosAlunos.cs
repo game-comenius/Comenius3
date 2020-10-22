9d1231d640cf655af067ef01538303bb2cf52df4
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-10-22 10:57:01 -0300

    Priorizar feedback (aluno) sobre mídia consulta

--- Assets/Scripts/Aula/PaginaResultadoDaAula/FeedbackDosAlunos.cs
+++ Assets/Scripts/Aula/PaginaResultadoDaAula/FeedbackDosAlunos.cs
@@ -13,40 +13,32 @@ public static class FeedbackDosAlunos
         }
         else // jogo.MetodologiaSelecionada == Metodologia.ABP
         {
-            var feedbacksPossiveis = new List<string>();
-
+            // Escolher um entre os feedbacks possíveis,
+            // dar prioridade a feedbacks sobre mídia de consulta
             var escolheuMidiaDeConsulta = jogo.MidiasSelecionadas.Any((midia) => midia.NomeMidia.CategoriasDaMidia().HasFlag(CategoriasDeMidia.ConsultaRepositorio));
             var escolheuMidiaDigital = jogo.MidiasSelecionadas.Any((midia) => midia.NomeMidia.CategoriasDaMidia().HasFlag(CategoriasDeMidia.Digital));
-
             if (jogo.NivelDeEnsinoSelecionado == NivelDeEnsino.EducacaoInfantil)
             {
-                if (escolheuMidiaDeConsulta) feedbacksPossiveis.Add("Adorei descobrir sobre tudo isso!");
-                if (escolheuMidiaDigital) feedbacksPossiveis.Add("É bem mais legal a aula com essas tecnologias.");
+                if (escolheuMidiaDeConsulta) return "Adorei descobrir sobre tudo isso!";
+                if (escolheuMidiaDigital) return "É bem mais legal a aula com essas tecnologias.";
             }
             else if (jogo.NivelDeEnsinoSelecionado == NivelDeEnsino.EnsinoFundamental)
             {
-                if (escolheuMidiaDeConsulta) feedbacksPossiveis.Add("Não imaginava que existia tanta coisa diferente sobre esse assunto!");
-                if (escolheuMidiaDigital) feedbacksPossiveis.Add("Gostei de interagir desse jeito na aula!");
+                if (escolheuMidiaDeConsulta) return "Não imaginava que existia tanta coisa diferente sobre esse assunto!";
+                if (escolheuMidiaDigital) return "Gostei de interagir desse jeito na aula!";
             }
             else if (jogo.NivelDeEnsinoSelecionado == NivelDeEnsino.EnsinoMedio)
             {
-                if (escolheuMidiaDeConsulta) feedbacksPossiveis.Add("Gostei de poder fazer as pesquisas sozinho, só pedi ajuda quando precisei mesmo.");
-                if (escolheuMidiaDigital) feedbacksPossiveis.Add("Usar essas ferramentas me ajudou na pesquisa e na apresentação!");
+                if (escolheuMidiaDeConsulta) return "Gostei de poder fazer as pesquisas sozinho, só pedi ajuda quando precisei mesmo.";
+                if (escolheuMidiaDigital) return "Usar essas ferramentas me ajudou na pesquisa e na apresentação!";
             }
             else // (jogo.NivelDeEnsinoSelecionado == NivelDeEnsino.EnsinoSuperior)
             {
-                if (escolheuMidiaDeConsulta) feedbacksPossiveis.Add("O professor nos deu autonomia para pesquisar e encontrar nossas próprias soluções.");
-                if (escolheuMidiaDigital) feedbacksPossiveis.Add("As mídias digitais permitem que a gente pesquise e se prepare muito mais.");
+                if (escolheuMidiaDeConsulta) return "O professor nos deu autonomia para pesquisar e encontrar nossas próprias soluções.";
+                if (escolheuMidiaDigital) return "As mídias digitais permitem que a gente pesquise e se prepare muito mais.";
             }
 
-            // Escolher um entre os feedbacks possíveis
-            if (feedbacksPossiveis.Any())
-            {
-                var randomIndex = Random.Range(0, feedbacksPossiveis.Count);
-                return feedbacksPossiveis.ElementAt(randomIndex);
-            }
-            else
-                return string.Empty;
+            return string.Empty;
         }
     }
 }
