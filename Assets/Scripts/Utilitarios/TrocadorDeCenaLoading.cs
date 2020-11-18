179ac25b43e484b24ee8e6db02c949cedf0fe16d
NicoleAGugli <38013926+nicolegugli@users.noreply.github.com>
2020-11-18 18:59:22 -0300

    Alterações no MenuPrincipal para cena de Loading

---
+++ Assets/Scripts/Utilitarios/TrocadorDeCenaLoading.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrocadorDeCenaLoading : MonoBehaviour
+{
+
+    static bool controleTrocaCena = false;
+
+    private void Awake()
+    {
+        if(controleTrocaCena == false)
+        {
+            SceneManager.LoadScene("Loading");
+            controleTrocaCena = true;
+        }
+    }
+
+}
