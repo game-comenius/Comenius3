cf49c6987230778fc230444b11c8489d82aa5fc1
bgduarte <bernardogomesduarte@gmail.com>
2021-04-08 12:29:02 -0300

    Feito script para mostrar outline quando mouse em cima

---
+++ Assets/Scripts/Aula/Quarto/OutlineOnHover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PolygonCollider2D))]
+public class OutlineOnHover : MonoBehaviour
+{
+    private Renderer rend;
+    
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void OnMouseEnter() 
+    {
+        Debug.Log("Entrou");
+        rend.sharedMaterial.SetFloat("_OutlineEnabled", 1f);
+    }
+
+    void OnMouseExit() 
+    {
+        Debug.Log("Saiu");
+        rend.sharedMaterial.SetFloat("_OutlineEnabled", 0f);
+    }
+}
