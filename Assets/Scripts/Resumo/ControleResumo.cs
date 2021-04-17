0e3116a38ea3ca293d685edf241056ec9012f9a0
Matheus-Aparicio <matheusilva-m@outlook.com>
2021-04-17 01:07:41 -0300

    melhora no resumo

---
+++ Assets/Scripts/Resumo/ControleResumo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleResumo : MonoBehaviour
+{
+
+    [SerializeField] GameObject PaginaResumoCriador;
+    [SerializeField] GameObject PaginaResumoPlanejamento;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var estadoDoJogo = EstadoDoJogo.Instance;
+
+        if (estadoDoJogo.MetodologiaSelecionada == Metodologia.ABP) {
+            PaginaResumoCriador.SetActive(true);
+            PaginaResumoPlanejamento.SetActive(false);
+        } else {
+            PaginaResumoCriador.SetActive(false);
+            PaginaResumoPlanejamento.SetActive(true);
+        }
+
+
+
+    }
+
+    /*
+    // Update is called once per frame
+    void Update()
+    {
+        PaginaResumoCriador.SetActive(false);
+    }
+    */
+}
---
+++ Assets/Scripts/Resumo/PaginaResumoDoPlanejamento.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PaginaResumoDoPlanejamento : Pagina
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var estadoDoJogo = EstadoDoJogo.Instance;
+        Debug.Log(estadoDoJogo.MidiasSelecionadas[0]);
+        /*
+        if (estadoDoJogo.NivelDeEnsinoSelecionado.Sprite)
+            iconeNivelDeEnsinoEscolhido.sprite = estadoDoJogo.NivelDeEnsinoSelecionado.Sprite;
+            nomeDoNivelDeEnsinoEscolhido.text = estadoDoJogo.NivelDeEnsinoSelecionado.nome;
+
+        if (estadoDoJogo.AreaDeConhecimentoSelecionada.Sprite)
+            iconeAreaDeConhecimentoEscolhida.sprite = estadoDoJogo.AreaDeConhecimentoSelecionada.Sprite;
+            nomeDaAreaDeConhecimentoEscolhida.text = estadoDoJogo.AreaDeConhecimentoSelecionada.nome;
+
+        if (estadoDoJogo.InteligenciasSelecionadas.SpriteGrande)
+            iconeInteligenciasEscolhidas.sprite = estadoDoJogo.InteligenciasSelecionadas.SpriteGrande;
+            nomeDasInteligenciasSelecionadas.text = estadoDoJogo.InteligenciasSelecionadas.Nome;
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
