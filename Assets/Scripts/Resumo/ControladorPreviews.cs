84243f75d04887ab9f2b207d20b0487dd405ff5d
Matheus-Aparicio <matheusilva-m@outlook.com>
2021-04-24 19:31:55 -0300

    novo layout no resumo e preview do quarto

---
+++ Assets/Scripts/Resumo/ControladorPreviews.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorPreviews : MonoBehaviour
+{
+
+    [SerializeField] private GameObject Infantil;
+    [SerializeField] private GameObject Fundamental;
+    [SerializeField] private GameObject Medio;
+    [SerializeField] private GameObject Superior;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Infantil.SetActive(false);
+        Fundamental.SetActive(false);
+        Medio.SetActive(false);
+        Superior.SetActive(false);
+
+        int indiceNivelDeEnsino = EstadoDoJogo.Instance.NivelDeEnsinoSelecionado.valor;
+
+        //Ativa o gameObject correspondente ao selecionado
+        switch (indiceNivelDeEnsino)
+        {
+            case 0: //Educação Infantil
+                Infantil.SetActive(true);
+                break;
+            case 1: //Ensino Fundamental
+                Fundamental.SetActive(true);
+                break;
+            case 2: //Ensino Medio
+                Medio.SetActive(true);
+                break;
+            default: //Ensino Superior
+                Superior.SetActive(true);
+                break;
+        }
+
+    }
+
+}
--- Assets/Scripts/Resumo/PaginaResumoDoPlanejamento.cs
+++ Assets/Scripts/Resumo/PaginaResumoDoPlanejamento.cs
@@ -9,10 +9,16 @@ public class PaginaResumoDoPlanejamento : Pagina
     [SerializeField] Image IconeMidia3;
     [SerializeField] Image IconeMidia4;
 
+    [Header("Previews")]
+    [SerializeField] Image PreviewQuarto1;
+    [SerializeField] Image PreviewQuarto2;
+
     [Header("Textos")]
     [SerializeField] Text NomeDaMetodologia;
-    [SerializeField] Text NomeDasMidias;
-    [SerializeField] Text NomeDoAgrupamento;
+    [SerializeField] Text NomeDaMidiaQuarto1;
+    [SerializeField] Text NomeDaMidiaQuarto2;
+    [SerializeField] Text NomeDaMidiaEAgrupamentoSala1;
+    [SerializeField] Text NomeDaMidiaEAgrupamentoSala2;
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +27,18 @@ public class PaginaResumoDoPlanejamento : Pagina
 
 
         NomeDaMetodologia.text = estadoDoJogo.MetodologiaSelecionada.nome;
-        NomeDasMidias.text = estadoDoJogo.MidiasSelecionadas[0].NomeApresentavel + ", " + estadoDoJogo.MidiasSelecionadas[1].NomeApresentavel +
-            ", " + estadoDoJogo.MidiasSelecionadas[2].NomeApresentavel + ", " + estadoDoJogo.MidiasSelecionadas[3].NomeApresentavel;
+        NomeDaMidiaQuarto1.text = estadoDoJogo.MidiasSelecionadas[0].NomeApresentavel;
+        NomeDaMidiaQuarto2.text = estadoDoJogo.MidiasSelecionadas[1].NomeApresentavel;
+        NomeDaMidiaEAgrupamentoSala1.text = estadoDoJogo.MidiasSelecionadas[2].NomeApresentavel + " + " + "Agrupamento 1";
+        NomeDaMidiaEAgrupamentoSala2.text = estadoDoJogo.MidiasSelecionadas[3].NomeApresentavel + " + " + "Agrupamento 2";
 
         IconeMidia1.sprite = estadoDoJogo.MidiasSelecionadas[0].SpriteIcone;
         IconeMidia2.sprite = estadoDoJogo.MidiasSelecionadas[1].SpriteIcone;
         IconeMidia3.sprite = estadoDoJogo.MidiasSelecionadas[2].SpriteIcone;
         IconeMidia4.sprite = estadoDoJogo.MidiasSelecionadas[3].SpriteIcone;
+
+        PreviewQuarto1.sprite = estadoDoJogo.NivelDeEnsinoSelecionado.Sprite;
+        PreviewQuarto2.sprite = estadoDoJogo.NivelDeEnsinoSelecionado.Sprite;
     }
 
 }
