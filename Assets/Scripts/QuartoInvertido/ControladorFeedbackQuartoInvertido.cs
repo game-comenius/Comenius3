a33318d088452900b117d06c74bec3e4e97ff7e5
Matheus-Aparicio <matheusilva-m@outlook.com>
2021-05-03 21:50:02 -0300

    feedback visual do quarto invertido funcionando

--- Assets/Scripts/QuartoInvertido/ControladorFeedbackQuartoInvertido.cs
+++ Assets/Scripts/QuartoInvertido/ControladorFeedbackQuartoInvertido.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using TMPro;
 using UnityEngine;
 
@@ -8,6 +9,10 @@ public class ControladorFeedbackQuartoInvertido : MonoBehaviour
 
     [SerializeField] GameObject feedbackVisual;
     [SerializeField] GameObject feedbackDescricao;
+
+    [Header("Retratos dos alunos")]
+    public Sprite alunoFeliz;
+    public Sprite alunoNeutro;
 
     // Start is called before the first frame update
     void Start()
@@ -23,15 +28,19 @@ public class ControladorFeedbackQuartoInvertido : MonoBehaviour
             {
                 case ("Corporal-cinestésica e Naturalista"):
                     feedbackDescricao.GetComponent<TextMeshProUGUI>().text = "Quero me movimentar, brincando lá fora.";
+                    feedbackVisual.GetComponent<Image>().sprite = alunoNeutro;
                     break;
                 case ("Intrapessoal e Espacial-visual"):
                     feedbackDescricao.GetComponent<TextMeshProUGUI>().text = "Gostei de fazer as tarefas e ver o conteúdo antes da aula.";
+                    feedbackVisual.GetComponent<Image>().sprite = alunoFeliz;
                     break;
                 case ("Interpessoal e Musical"):
                     feedbackDescricao.GetComponent<TextMeshProUGUI>().text = "Quero fazer as tarefas ouvindo música com os meus colegas.";
+                    feedbackVisual.GetComponent<Image>().sprite = alunoNeutro;
                     break;
                 case ("Linguística e Lógico-matemática"):
                     feedbackDescricao.GetComponent<TextMeshProUGUI>().text = "Quero aprender com as histórias contadas pelo professor.";
+                    feedbackVisual.GetComponent<Image>().sprite = alunoNeutro;
                     break;
                 default:
                     feedbackDescricao.GetComponent<TextMeshProUGUI>().text = "Não foi possível obter a inteligência selecionada.";
@@ -45,15 +54,19 @@ public class ControladorFeedbackQuartoInvertido : MonoBehaviour
             {
                 case ("Corporal-cinestésica e Naturalista"):
                     feedbackDescricao.GetComponent<TextMeshProUGUI>().text = "Gostaria de realizar atividades a céu aberto, em contato com a natureza";
+                    feedbackVisual.GetComponent<Image>().sprite = alunoNeutro;
                     break;
                 case ("Intrapessoal e Espacial-visual"):
                     feedbackDescricao.GetComponent<TextMeshProUGUI>().text = "Adorei a ideia de poder entender e visualizar o conteúdo primeiro com atividades individuais.";
+                    feedbackVisual.GetComponent<Image>().sprite = alunoFeliz;
                     break;
                 case ("Interpessoal e Musical"):
                     feedbackDescricao.GetComponent<TextMeshProUGUI>().text = "Gostaria de ter debates e diálogos sobre o que estudei em casa.";
+                    feedbackVisual.GetComponent<Image>().sprite = alunoNeutro;
                     break;
                 case ("Linguística e Lógico-matemática"):
                     feedbackDescricao.GetComponent<TextMeshProUGUI>().text = "Quero poder entender o conteúdo, tirando dúvidas e fazendo as minhas anotações.";
+                    feedbackVisual.GetComponent<Image>().sprite = alunoNeutro;
                     break;
                 default:
                     feedbackDescricao.GetComponent<TextMeshProUGUI>().text = "Não foi possível obter a inteligência selecionada.";
