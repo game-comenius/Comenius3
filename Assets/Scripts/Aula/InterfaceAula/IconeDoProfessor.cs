b428f124da3af2b30a8d3ce7c76f1b646b06d57b
bgduarte <bernardogomesduarte@gmail.com>
2021-06-07 15:39:18 -0300

    Correcao do icone do professor nao exibir corretamente na sala de aula invertida

---
+++ Assets/Scripts/Aula/InterfaceAula/IconeDoProfessor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconeDoProfessor : MonoBehaviour
+{
+    Image imagemProfessor;
+
+    private void Start()
+    {
+        if (imagemProfessor == null)
+            imagemProfessor = transform.gameObject.GetComponent<Image>();
+
+        imagemProfessor.sprite = EstadoDoJogo.Instance.SpriteIconePersonagem;
+    }
+
+}
