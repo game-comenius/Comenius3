f80081451422a00eeca3d58942acbbeef064e6ba
bgduarte <bernardogomesduarte@gmail.com>
2021-06-22 16:04:29 -0300

    Adicionado um indicador de afirmativa correta/falsa para um dos quizzes

--- Assets/Scripts/Aula/Quiz/AfirmacaoQuiz.cs
+++ Assets/Scripts/Aula/Quiz/AfirmacaoQuiz.cs
@@ -6,6 +6,7 @@ public abstract class AfirmacaoQuiz : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] Sprite spriteErrada;
     [SerializeField] Sprite spriteCorreta;
+    [SerializeField] Text marcacaoResposta;
 
     [SerializeField] Text textoAfirmacao;
 
@@ -29,7 +30,32 @@ public abstract class AfirmacaoQuiz : MonoBehaviour, IPointerClickHandler
 
     public abstract void OnPointerClick(PointerEventData eventData);
 
-    public virtual void DefinirComoAcertoDoJogador() { GetComponent<Image>().sprite = spriteCorreta; }
+    public virtual void DefinirComoAcertoDoJogador()
+    {
+        GetComponent<Image>().sprite = spriteCorreta;
+        MostrarResposta();
+    }
 
-    public virtual void DefinirComoErroDoJogador() { GetComponent<Image>().sprite = spriteErrada; }
+    public virtual void DefinirComoErroDoJogador()
+    {
+        GetComponent<Image>().sprite = spriteErrada;
+        MostrarResposta();
+    }
+    protected virtual void MostrarResposta()
+    {
+        if(marcacaoResposta != null)
+        {
+            if(Afirmacao.Verdadeira)
+            {
+                marcacaoResposta.enabled = true;
+                marcacaoResposta.color = Color.green;
+                marcacaoResposta.text = "V";
+            } else
+            {
+                marcacaoResposta.enabled = true;
+                marcacaoResposta.color = Color.red;
+                marcacaoResposta.text = "X";
+            }
+        }
+    }
 }
