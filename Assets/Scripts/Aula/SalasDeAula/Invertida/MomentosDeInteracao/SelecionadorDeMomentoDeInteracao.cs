36018070d15cb986a50d008ade706e074bb9b926
bgduarte <57222160+bgduarte@users.noreply.github.com>
2021-07-02 13:57:54 -0300

    Adicionado os momentos de interacao inicias que estavam na planilha

---
+++ Assets/Scripts/Aula/SalasDeAula/Invertida/MomentosDeInteracao/SelecionadorDeMomentoDeInteracao.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ControladorDisplayMomentoInteracao))]
+public class SelecionadorDeMomentoDeInteracao : MonoBehaviour
+{
+    [SerializeField] List<MomentoInteracao> momentos;
+    [SerializeField] List<MomentoInteracao> momentosInfantil;
+    [SerializeField] ControladorDisplayMomentoInteracao controlador;
+
+    private void OnEnable()
+    {
+        controlador = gameObject.GetComponent<ControladorDisplayMomentoInteracao>();
+        SelecionarMomento();
+    }
+
+   private void SelecionarMomento()
+   {
+        NivelDeEnsino nivel = EstadoDoJogo.Instance.NivelDeEnsinoSelecionado;
+        if(nivel == NivelDeEnsino.EducacaoInfantil)
+            controlador.Momento = momentosInfantil[Random.Range(0, momentosInfantil.Count)];
+        else
+            controlador.Momento = momentos[Random.Range(0, momentos.Count)];
+   }
+
+}
