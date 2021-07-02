3d5376d1111d330eaacedab35118b8a97e83f295
bgduarte <57222160+bgduarte@users.noreply.github.com>
2021-07-02 12:06:13 -0300

    Adicionado Scriptable Object para criacao de flags de estado de jogo

---
+++ Assets/Scripts/Administracao/FlagsDeEstadoDeJogo/FlagDeEstadoDeJogo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "MomentoInteracao", menuName = "Comenius3/FlagDeEstadoDeJogo", order = 0)]
+public class FlagDeEstadoDeJogo : ScriptableObject
+{
+    public bool value;
+}
