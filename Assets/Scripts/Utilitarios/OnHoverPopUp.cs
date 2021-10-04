75da47cb6267ed80cc9f1ef72ffaf8e6ad637842
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2021-10-04 14:19:24 -0300

    Hover aprimorado

--- Assets/Scripts/Utilitarios/OnHoverPopUp.cs
+++ Assets/Scripts/Utilitarios/OnHoverPopUp.cs
@@ -8,22 +8,63 @@ public class OnHoverPopUp : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 {
     public GameObject popUp;
     public Text text;
-    public Vector3 popUpOffset;  // Offset a partir do centro do objeto selecionado
+    public Orientation orientation;
     [TextArea]
     public string content;
 
+    public enum Orientation
+    {
+        Top,
+        Bottom,
+        Right,
+        Left
+    }
+
     private RectTransform rect;
     private RectTransform popUpRect;
+    private Vector3 popUpPosition;
+    private Vector3[] worldCorners;
+    private Vector3[] popUpWorldCorners;
 
     void Start()
     {
         rect = GetComponent<RectTransform>();
         popUpRect = popUp.GetComponent<RectTransform>();
+
+        // Obtém a partir do canto inferior esquerdo e vai até o canto inferior direito.
+        worldCorners = new Vector3[4];
+        popUpWorldCorners = new Vector3[4];
+        rect.GetWorldCorners(worldCorners);
+        popUpRect.GetWorldCorners(popUpWorldCorners);
+
+        float halfHeight = Mathf.Abs(rect.position.y - worldCorners[0].y);
+        float halfWidth = Mathf.Abs(rect.position.x - worldCorners[0].x);
+
+        float halfPopUpHeight = Mathf.Abs(popUpRect.position.y - popUpWorldCorners[0].y);
+        float halfPopUpWidth = Mathf.Abs(popUpRect.position.x - popUpWorldCorners[0].x);
+
+        switch (orientation)
+        {
+            case Orientation.Top:
+                popUpPosition = new Vector3(rect.position.x, rect.position.y + halfHeight + halfPopUpHeight, 0f);
+                break;
+            case Orientation.Bottom:
+                popUpPosition = new Vector3(rect.position.x, rect.position.y - (halfHeight + halfPopUpHeight), 0f);
+                break;
+            case Orientation.Right:
+                popUpPosition = new Vector3(rect.position.x + halfWidth + halfPopUpWidth, rect.position.y, 0f);
+                break;
+            case Orientation.Left:
+                popUpPosition = new Vector3(rect.position.x - (halfWidth + halfPopUpWidth), rect.position.y, 0f);
+                break;
+            default:
+                break;
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        popUpRect.position = rect.position + popUpOffset;
+        popUpRect.position = popUpPosition;
         popUp.SetActive(true);
         text.text = content;
     }
