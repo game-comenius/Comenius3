13d75774904ca5240187e8a4dcb1d42b4a8c5312
Matheus <matheusilva-m@outlook.com>
2021-10-11 18:47:45 -0300

    novo script para transformar objetos e correcao da layer que o botao menu se encontrava

---
+++ Assets/Scripts/Utilitarios/TransformadorDeObjeto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformadorDeObjeto : MonoBehaviour
+{
+
+    public GameObject objeto;
+
+
+    public void MudarPosicaoX(int x)
+    {
+        objeto.transform.position = new Vector3((float)x, objeto.transform.position.y, objeto.transform.position.z);
+    }
+
+    public void MudarPosicaoY(int y)
+    {
+        objeto.transform.position = new Vector3(objeto.transform.position.x, (float)y, objeto.transform.position.z);
+    }
+}
