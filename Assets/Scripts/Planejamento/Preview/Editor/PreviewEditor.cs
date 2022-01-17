f2cfd8a997379701ea11a75636ad4d6f2246a669
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2022-01-17 16:22:57 -0300

    v2.1-dev: Preview dos resumo refeito.

---
+++ Assets/Scripts/Planejamento/Preview/Editor/PreviewEditor.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+[CustomEditor(typeof(Preview))]
+public class PreviewEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var previewScript = target as Preview;
+
+        string[] options = new string[]
+        {
+            "Preview de quartos", "Preview de salas Simples", "Preview de salas com Agrupamentos"
+        };
+
+        previewScript.selectedOption = EditorGUILayout.Popup("Opção", previewScript.selectedOption, options);
+
+        switch (previewScript.selectedOption)
+        {
+            case 0:
+                previewScript.quartoInfantil = EditorGUILayout.ObjectField("Quarto Infantil",
+                                                                           previewScript.quartoInfantil,
+                                                                           typeof(Sprite),
+                                                                           true) as Sprite;
+
+                previewScript.quartoFundamental = EditorGUILayout.ObjectField("Quarto Fundamental",
+                                                                           previewScript.quartoFundamental,
+                                                                           typeof(Sprite),
+                                                                           true) as Sprite;
+
+                previewScript.quartoMedio = EditorGUILayout.ObjectField("Quarto Médio",
+                                                                           previewScript.quartoMedio,
+                                                                           typeof(Sprite),
+                                                                           true) as Sprite;
+
+                previewScript.quartoSuperior = EditorGUILayout.ObjectField("Quarto Superior",
+                                                                           previewScript.quartoSuperior,
+                                                                           typeof(Sprite),
+                                                                           true) as Sprite;
+                break;
+            case 1:
+                previewScript.salaInfantilIndividual = EditorGUILayout.ObjectField("Sala Infantil",
+                                                                           previewScript.salaInfantilIndividual,
+                                                                           typeof(Sprite),
+                                                                           true) as Sprite;
+
+                previewScript.salaRegularIndividual = EditorGUILayout.ObjectField("Sala Regular",
+                                                                           previewScript.salaRegularIndividual,
+                                                                           typeof(Sprite),
+                                                                           true) as Sprite;
+                break;
+            case 2:
+                previewScript.salaInfantilU = EditorGUILayout.ObjectField("Sala Infantil U",
+                                                                           previewScript.salaInfantilU,
+                                                                           typeof(Sprite),
+                                                                           true) as Sprite;
+
+                previewScript.salaInfantilIndividual = EditorGUILayout.ObjectField("Sala Infantil Individual",
+                                                                           previewScript.salaInfantilIndividual,
+                                                                           typeof(Sprite),
+                                                                           true) as Sprite;
+
+                previewScript.salaInfantilDuplasETrios = EditorGUILayout.ObjectField("Sala Infantil Duplas e Trios",
+                                                                           previewScript.salaInfantilDuplasETrios,
+                                                                           typeof(Sprite),
+                                                                           true) as Sprite;
+
+                previewScript.salaInfantilGrandesGrupos = EditorGUILayout.ObjectField("Sala Infantil Grandes Grupos",
+                                                                           previewScript.salaInfantilGrandesGrupos,
+                                                                           typeof(Sprite),
+                                                                           true) as Sprite;
+
+                previewScript.salaRegularU = EditorGUILayout.ObjectField("Sala Regular U",
+                                                                           previewScript.salaRegularU,
+                                                                           typeof(Sprite),
+                                                                           true) as Sprite;
+
+                previewScript.salaRegularIndividual = EditorGUILayout.ObjectField("Sala Regular Individual",
+                                                                           previewScript.salaRegularIndividual,
+                                                                           typeof(Sprite),
+                                                                           true) as Sprite;
+
+                previewScript.salaRegularTrios = EditorGUILayout.ObjectField("Sala Regular Duplas e Trios",
+                                                                           previewScript.salaRegularTrios,
+                                                                           typeof(Sprite),
+                                                                           true) as Sprite;
+
+                previewScript.salaRegularGrandesGrupos = EditorGUILayout.ObjectField("Sala Regular Grandes Grupos",
+                                                                           previewScript.salaRegularGrandesGrupos,
+                                                                           typeof(Sprite),
+                                                                           true) as Sprite;
+
+                previewScript.segundoAgrupamento = EditorGUILayout.Toggle("Segundo Agrupamento",
+                                                                          previewScript.segundoAgrupamento);
+                break;
+            default:
+                Debug.LogWarning("Opção inválida no preview de quartos e salas");
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Planejamento/Preview/Preview.cs
@@ -0,0 +1,120 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class Preview : MonoBehaviour
+{
+    [HideInInspector] public int selectedOption;
+    [HideInInspector] public Sprite quartoInfantil;
+    [HideInInspector] public Sprite quartoFundamental;
+    [HideInInspector] public Sprite quartoMedio;
+    [HideInInspector] public Sprite quartoSuperior;
+    [HideInInspector] public Sprite salaInfantilU;
+    [HideInInspector] public Sprite salaInfantilIndividual;
+    [HideInInspector] public Sprite salaInfantilDuplasETrios;
+    [HideInInspector] public Sprite salaInfantilGrandesGrupos;
+    [HideInInspector] public Sprite salaRegularU;
+    [HideInInspector] public Sprite salaRegularIndividual;
+    [HideInInspector] public Sprite salaRegularTrios;
+    [HideInInspector] public Sprite salaRegularGrandesGrupos;
+    [HideInInspector] public bool segundoAgrupamento;
+
+    private Image imagemAlvo;
+
+    private void OnEnable()
+    {
+        if (imagemAlvo == null)
+        {
+            imagemAlvo = GetComponent<Image>();
+        }
+
+        switch (selectedOption)
+        {
+            case 0:
+
+                if (EstadoDoJogo.Instance.NivelDeEnsinoSelecionado == NivelDeEnsino.EducacaoInfantil)
+                {
+                    imagemAlvo.sprite = quartoInfantil;
+                }
+                else if (EstadoDoJogo.Instance.NivelDeEnsinoSelecionado == NivelDeEnsino.EnsinoFundamental)
+                {
+                    imagemAlvo.sprite = quartoFundamental;
+                }
+                else if (EstadoDoJogo.Instance.NivelDeEnsinoSelecionado == NivelDeEnsino.EnsinoMedio)
+                {
+                    imagemAlvo.sprite = quartoMedio;
+                }
+                else
+                {
+                    imagemAlvo.sprite = quartoSuperior;
+                }
+
+                break;
+            case 1:
+
+                if (EstadoDoJogo.Instance.NivelDeEnsinoSelecionado == NivelDeEnsino.EducacaoInfantil)
+                {
+                    imagemAlvo.sprite = salaInfantilIndividual;
+                }
+                else
+                {
+                    imagemAlvo.sprite = salaRegularIndividual;
+                }
+
+                break;
+            case 2:
+
+                if (EstadoDoJogo.Instance.NivelDeEnsinoSelecionado == NivelDeEnsino.EducacaoInfantil)
+                {
+                    int indice = segundoAgrupamento ? 3 : 2;
+
+                    switch (EstadoDoJogo.Instance.MidiasSelecionadas[indice].agrupamento)
+                    {
+                        case Agrupamento.FormatoU:
+                            imagemAlvo.sprite = salaInfantilU;
+                            break;
+                        case Agrupamento.Individual:
+                            imagemAlvo.sprite = salaInfantilIndividual;
+                            break;
+                        case Agrupamento.PequenosGrupos:
+                            imagemAlvo.sprite = salaInfantilDuplasETrios;
+                            break;
+                        case Agrupamento.GrandesGrupos:
+                            imagemAlvo.sprite = salaInfantilGrandesGrupos;
+                            break;
+                        default:
+                            Debug.LogError("Agrupamento inválido");
+                            break;
+                    }
+                }
+                else
+                {
+                    int indice = segundoAgrupamento ? 3 : 2;
+
+                    switch (EstadoDoJogo.Instance.MidiasSelecionadas[indice].agrupamento)
+                    {
+                        case Agrupamento.FormatoU:
+                            imagemAlvo.sprite = salaRegularU;
+                            break;
+                        case Agrupamento.Individual:
+                            imagemAlvo.sprite = salaRegularIndividual;
+                            break;
+                        case Agrupamento.PequenosGrupos:
+                            imagemAlvo.sprite = salaRegularTrios;
+                            break;
+                        case Agrupamento.GrandesGrupos:
+                            imagemAlvo.sprite = salaRegularGrandesGrupos;
+                            break;
+                        default:
+                            Debug.LogError("Agrupamento inválido");
+                            break;
+                    }
+                }
+
+                break;
+            default:
+                Debug.LogWarning("Falha na opção selecionada.");
+                break;
+        }
+    }
+}
