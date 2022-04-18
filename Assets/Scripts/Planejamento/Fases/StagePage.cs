6872cf290ee9e97f5492ea13d15c48b4231d8495
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2022-04-18 18:51:48 -0300

    Now the methodology is updated with the stage change

---
+++ Assets/Scripts/Planejamento/Fases/StagePage.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class StagePage : PaginaPlanejamento
+{
+    [SerializeField] private Methodology methodology;
+    [SerializeField] private Sprite methodologySprite;
+
+    private enum Methodology
+    {
+        ABP,
+        SAI,
+        ABProj
+    }
+
+    protected override void OnEnable()
+    {
+        fundo.sprite = spriteFundo;
+        textoAjuda.text = ajuda;
+
+        iconManager.ShowIcon(0);
+        iconManager.HideIcon(1);
+        iconManager.HideIcon(2);
+        iconManager.HideIcon(3);
+
+        iconManager.SetIcon(0, methodologySprite);
+    }
+
+    private void OnDisable()
+    {
+        iconManager.ResetIcon(0);
+    }
+
+    private void Start()
+    {
+        switch (methodology)
+        {
+            case Methodology.ABP:
+                EstadoDoJogo.Instance.Metodologia = Metodologia.Get(0);
+                break;
+            case Methodology.SAI:
+                EstadoDoJogo.Instance.Metodologia = Metodologia.Get(1);
+                break;
+            case Methodology.ABProj:
+                EstadoDoJogo.Instance.Metodologia = Metodologia.Get(2);
+                break;
+            default:
+                Debug.LogError("Erro na definição da metodologia");
+                break;
+        }
+
+        EstadoDoJogo.Instance.Metodologia.sprite = methodologySprite;
+    }
+}
