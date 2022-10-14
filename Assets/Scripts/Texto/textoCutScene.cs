7f64f0d59b6eb34043063317ab3a1ffd68cf5f82
Baillo <luccasbaillo@gmail.com>
2022-10-14 13:20:06 -0300

    att

--- Assets/Scripts/Texto/textoCutScene.cs
+++ Assets/Scripts/Texto/textoCutScene.cs
@@ -29,12 +29,12 @@ public class textoCutScne : MonoBehaviour
         textosCutscene[0] = "Olá, Jogador(a).\n Eu sou o Comenius, o pai da Didática. \n Vou te mostrar como planejar melhor suas \n aulas com as mídias. ";
         textosCutscene[1] = "E eu sou a professora Maria de Lourdes,\n mas pode me chamar de Lurdinha.";
         textosCutscene[2] = "Venho aprendendo muito sobre os usos das novas \nmídias nos planejamentos escolares, e vou ajudar você\n nessa missão!";
-        textosCutscene[3] = "Precisamos criar práticas didáticas mais ágeis e\n tornar os estudantes protagonistas no processo. \nDurante o jogo serão apresentadas três metodologias ativas que vão te ajudar nesta tarefa.";
+        textosCutscene[3] = "Precisamos criar práticas didáticas mais ágeis e\n tornar os estudantes protagonistas no processo. \nDurante o jogo serão apresentadas três metodologias\n ativas que vão te ajudar nesta tarefa.";
         textosCutscene[4] = "Aqui no jogo você vai estender os espaços de\n aprendizagem para além das paredes da sala de aula e dos muros da escola.";
         textosCutscene[5] = "Para isso, você vai poder utilizar os laboratórios, instigar os \n estudantes a pesquisar, realizar partes das atividades \n em casa, levá-los à cidade para aprender de maneira \nenvolvente e a partir de diferentes contextos.";
         textosCutscene[6] = "Aqui você vai criar sequências didáticas com as mídias\n do século XXI, planejando com metodologias ativas e \n considerando as inteligências múltiplas das suas turmas.";
         textosCutscene[7] = "Você está pronto(a) para começar \nnossa jornada?";
-        textosCutscene[8] = "<u><b>Ótimo!</b></u> Então  vamos juntos explorar o potencial e o \n desafio de incluir as mídias digitais na educação. \nVamos lá?";
+        textosCutscene[8] = "<b>Ótimo!</b> Então  vamos juntos explorar o potencial e o \n desafio de incluir as mídias digitais na educação. \nVamos lá?";
 
         textosMostrados[0].text = textosCutscene[paginaAtual];
     }
